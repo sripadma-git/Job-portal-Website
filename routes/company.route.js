24c93aa9be497fbdfe52a09f68033ae88a5f92ac
Sri Padma Chintha <144418506+sripadma-git@users.noreply.github.com>
2025-05-19 16:12:10 +0530

    Add files via upload

---
+++ routes/company.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { getCompany, getCompanyById, registerCompany, updateCompany } from "../controllers/company.controllers.js";
+
+const router = express.Router();
+
+router.route("/register").post(isAuthenticated,registerCompany);
+router.route("/get").get(isAuthenticated,getCompany);
+router.route("/get/:id").get(isAuthenticated,getCompanyById);
+router.route("/update/:id").put(isAuthenticated, updateCompany);
+
+export default router;
